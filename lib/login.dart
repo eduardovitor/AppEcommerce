c59327ae0e2cabc6d0cf34ccef96d4d1c055a233
Eduardo Vitor <eduardovitor730@gmail.com>
2022-09-18 15:28:28 -0300

    Login apenas com usuário do banco

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:app_ecommerce/constantes.dart';
 import 'package:app_ecommerce/custom_icons_icons.dart';
+import 'package:app_ecommerce/usuario_controller.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -108,7 +109,9 @@ class MeuForm extends StatefulWidget {
 }
 
 class _MeuFormState extends State<MeuForm> {
-  final _formkey = GlobalKey<FormState>;
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  TextEditingController controllerEmail = TextEditingController();
+  TextEditingController controllerSenha = TextEditingController();
   // void pressionarBotaoOcultarSenha(){
   //  setState(() {
   //      = indice; adicionar uma variável e colocar if ternário
@@ -117,43 +120,54 @@ class _MeuFormState extends State<MeuForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
+        key: _formkey,
         child: Expanded(
-      child: ListView(children: [
-        TextFormField(
-          decoration: InputDecoration(
-              suffixIcon: const Icon(Icons.email),
-              labelText: 'Email',
-              hintText: 'Digite o seu email',
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(30))),
-        ),
-        const SizedBox(height: 15),
-        TextFormField(
-          obscureText: true,
-          decoration: InputDecoration(
-              suffixIcon: const Icon(Icons.password),
-              labelText: 'Senha',
-              hintText: 'Digite a sua senha',
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(30))),
-        ),
-        const SizedBox(height: 15),
-        buildRowOpcoes(),
-        const SizedBox(height: 30),
-        ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                minimumSize: const Size(120, 40),
-                primary: corPrimaria,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30))),
-            onPressed: () {
-              Navigator.pushNamed(context, '/home');
-            },
-            child: const Text('Entrar')),
-        const SizedBox(height: 20),
-        buildRowRedesSociais(),
-        buildRowBotaoCadastrar(context)
-      ]),
-    ));
+          child: ListView(children: [
+            TextFormField(
+              controller: controllerEmail,
+              decoration: InputDecoration(
+                  suffixIcon: const Icon(Icons.email),
+                  labelText: 'Email',
+                  hintText: 'Digite o seu email',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30))),
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              controller: controllerSenha,
+              obscureText: true,
+              decoration: InputDecoration(
+                  suffixIcon: const Icon(Icons.password),
+                  labelText: 'Senha',
+                  hintText: 'Digite a sua senha',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30))),
+            ),
+            const SizedBox(height: 15),
+            buildRowOpcoes(),
+            const SizedBox(height: 30),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(120, 40),
+                    primary: corPrimaria,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30))),
+                onPressed: entrar,
+                child: const Text('Entrar')),
+            const SizedBox(height: 20),
+            buildRowRedesSociais(),
+            buildRowBotaoCadastrar(context)
+          ]),
+        ));
+  }
+
+  void entrar() async {
+    bool eValido = _formkey.currentState!.validate();
+    bool usuarioExiste = await UsuarioController()
+        .login(controllerEmail.text, controllerSenha.text);
+    if (eValido && usuarioExiste) {
+      // ignore: use_build_context_synchronously
+      Navigator.pushNamed(context, '/home');
+    }
   }
 }
