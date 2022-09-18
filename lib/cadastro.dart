48b98d7c62b980e923ef658ec0fe03167e9a1c4f
Eduardo Vitor <eduardovitor730@gmail.com>
2022-09-18 15:52:25 -0300

    Adição do cadastro de usuários no bd

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -1,4 +1,6 @@
 import 'package:app_ecommerce/constantes.dart';
+import 'package:app_ecommerce/usuario.dart';
+import 'package:app_ecommerce/usuario_controller.dart';
 import 'package:flutter/material.dart';
 
 import 'login.dart';
@@ -42,49 +44,84 @@ class MyForm extends StatefulWidget {
 }
 
 class _MyFormState extends State<MyForm> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  TextEditingController controllerNome = TextEditingController();
+  TextEditingController controllerEmail = TextEditingController();
+  TextEditingController controllerSenha = TextEditingController();
+  TextEditingController controllerConfSenha = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Form(
+        key: _formkey,
         child: Expanded(
             child: ListView(children: [
-      TextFormField(
-        decoration: InputDecoration(
-            suffixIcon: const Icon(Icons.email),
-            labelText: 'Email',
-            hintText: 'Digite o seu email',
-            border:
-                OutlineInputBorder(borderRadius: BorderRadius.circular(30))),
-      ),
-      const SizedBox(height: 15),
-      TextFormField(
-        obscureText: true,
-        decoration: InputDecoration(
-            suffixIcon: const Icon(Icons.password),
-            labelText: 'Senha',
-            hintText: 'Digite a sua senha',
-            border:
-                OutlineInputBorder(borderRadius: BorderRadius.circular(30))),
-      ),
-      const SizedBox(height: 15),
-      TextFormField(
-        obscureText: true,
-        decoration: InputDecoration(
-            suffixIcon: const Icon(Icons.password),
-            labelText: 'Confirme a senha',
-            hintText: 'Digite sua senha novamente',
-            border:
-                OutlineInputBorder(borderRadius: BorderRadius.circular(30))),
-      ),
-      const SizedBox(height: 30),
-      ElevatedButton(
-          style: ElevatedButton.styleFrom(
-              minimumSize: const Size(120, 40),
-              primary: corPrimaria,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30))),
-          onPressed: () {},
-          child: const Text('Entrar')),
-      buildRowRedesSociais()
-    ])));
+          TextFormField(
+            controller: controllerNome,
+            decoration: InputDecoration(
+                suffixIcon: const Icon(Icons.person),
+                labelText: 'Nome',
+                hintText: 'Digite o seu nome',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30))),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            controller: controllerEmail,
+            decoration: InputDecoration(
+                suffixIcon: const Icon(Icons.email),
+                labelText: 'Email',
+                hintText: 'Digite o seu email',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30))),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            controller: controllerSenha,
+            obscureText: true,
+            decoration: InputDecoration(
+                suffixIcon: const Icon(Icons.password),
+                labelText: 'Senha',
+                hintText: 'Digite a sua senha',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30))),
+          ),
+          const SizedBox(height: 15),
+          TextFormField(
+            controller: controllerConfSenha,
+            obscureText: true,
+            decoration: InputDecoration(
+                suffixIcon: const Icon(Icons.password),
+                labelText: 'Confirme a senha',
+                hintText: 'Digite sua senha novamente',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30))),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(120, 40),
+                  primary: corPrimaria,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30))),
+              onPressed: cadastrar,
+              child: const Text('Entrar')),
+          buildRowRedesSociais()
+        ])));
+  }
+
+  void cadastrar() async {
+    Usuario usuario = Usuario(
+        email: controllerEmail.text,
+        nome: controllerNome.text,
+        senha: controllerSenha.text);
+    // ignore: unrelated_type_equality_checks
+    bool senhaValida = controllerSenha.text == controllerConfSenha.text;
+    bool isValid = _formkey.currentState!.validate();
+    bool usuarioCadastrado =
+        await UsuarioController().cadastrarUsuario(usuario);
+    if (isValid && senhaValida && usuarioCadastrado) {
+      // ignore: use_build_context_synchronously
+      Navigator.pushNamed(context, '/home');
+    }
   }
 }
